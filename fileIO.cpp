9950b3b28b8e951bc5f623b4c8640f2e5a8c89c4
ellaccrites <ellac.crites2006@gmail.com>
2026-02-12 04:41:42 +0000

    fileIo.cpp working

--- fileIO.cpp
+++ fileIO.cpp
@@ -29,7 +29,6 @@ int main(){
 		getline(ss, sNum1, ',');
 		intConvert << sNum1;
 		intConvert >> num1;
-		std::cout << "Num1: " << num1 << "-";
 
 		intConvert.clear();
                 intConvert.str("");
@@ -37,32 +36,15 @@ int main(){
 		getline(ss, sNum2, ',');
 		intConvert << sNum2;
 		intConvert >> num2;
-		std::cout << "Num2: " << num2 << "-";
 		getline(ss, text);
 
 		int total = num1 + num2;
-		std::cout << "Total: " << total << std::endl;
-		/*
 		for(int i = 0; i < total; i++){
 			std::cout << text << "  ";
 		} // end for
-		*/
 		std::cout << std::endl;
 	} // end while
 	
-/*
-	while(getline(inFile, currentLine)){
-		std::cout << "Line: " << currentLine << std::endl;
-	 	ss.clear();
-                ss.str("");
-
-                ss.str(currentLine);
-		getline(ss, sNum1, ',');
-		std::cout << "Number 1: " << sNum1 << std::endl;
-		getline(ss, sNum2, ',');
-		std::cout << "Number 2: " << sNum2 << std::endl;
-	}
-*/
 	inFile.close();
 
 } // end main
